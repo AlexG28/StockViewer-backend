339800e4e471df2edfacb6333d8c723460e12fe3
Alex <alex.gyumushyan@gmail.com>
2021-05-07 23:04:09 -0400

    minor changes

--- routes/getQuotesRoute.js
+++ routes/getQuotesRoute.js
@@ -5,17 +5,13 @@ const schema = require('../models/Defaults');
 // need one to get back all the categories of stocks (everything)
 
 router.get('/', async (req,res) => {
-
     try{
         const response = await schema.find();
         res.json(response);
     } catch {
-
         res.json({message: err});
     }
-
-
-})
+});
 
 
 
