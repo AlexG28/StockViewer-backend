8acede2e28ddcc00a25dbb6f85b4d66cfce794b9
Alex <alex.gyumushyan@gmail.com>
2022-01-23 13:15:11 -0500

    added app.post in index but can't use find() on database

--- index.js
+++ index.js
@@ -17,10 +17,14 @@ app.use((req,res,next) => {
     next();
 });
 
+
 mongoose.connect (
     process.env.DB_USER,
     () => console.log("Connected to database!")
 );
+
+
+
 
 
 const getQuoteRoute = require('./routes/getQuotesRoute');
@@ -31,6 +35,19 @@ app.get("/", (req,res) => {
     //res.send("hello world");
     res.json({test: 'This clearly works'});
     console.log("test1");
+});
+
+app.post("/test", async (req, res) => {
+    //await mongoose.connect(process.env.DB_USER, () => console.log("connected!!!"));
+    
+
+    const customerSchema = new mongoose.Schema({name: String, age: Number, email: String});
+    const Customer = mongoose.model('Customer', customerSchema);
+
+    await Customer.create({name: 'Alex', age: 19, email: 'alex@gmail.com'});
+    await Customer.create({name: 'Noura', age: 20, email: 'noura@gmail.com'});
+    //const allCustomers = await Customer.find();
+    console.log('I think this worked?????');
 });
 
 
